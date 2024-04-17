b26048fa1ce7f44bbfe3ceb18f0b35a5c3edf433
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-04-17 20:07:54 -0300

    vau

--- lista_de_tarefas/lib/pages/login_page.dart
+++ lista_de_tarefas/lib/pages/login_page.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flare_flutter/flare_actor.dart';
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final String googleImage = 'assets/imagens/google.svg';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 24, 48, 59),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+                  Icons.check_circle,
+                  color: Colors.white,
+                  size: 100,
+                ),
+                SizedBox(height: 20,),
+            Text(
+              "Bem vindo(a) ao Taskify",
+              style: GoogleFonts.barlowCondensed(
+                textStyle: TextStyle(color: Colors.white),
+                fontSize: 29,
+              ),
+            ),
+            
+            SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 50,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  // Função a ser executada ao pressionar o botão
+                },
+
+                icon: SvgPicture.asset('assets/imagens/google.svg', width: 24, height: 24,),
+                label: Text(
+                  "Continuar com o Google",
+                  style: TextStyle(color: Colors.black, fontSize: 21),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white, // Cor do botão
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lista_de_tarefas/lib/pages/splash_screen.dart
+++ lista_de_tarefas/lib/pages/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/pages/login_page.dart';
 import 'package:lottie/lottie.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,12 +12,23 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    // Define um temporizador para redirecionar após 3 segundos
+    Timer(Duration(seconds: 4), () {
+      // Substitui a tela atual pela página de login e remove a rota anterior
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 24, 48, 59),
       body: Center(
-        
-        child: Lottie.network('https://lottie.host/8f386b7d-080a-4651-99b6-29e18602f5e6/4TtQv5sA8X.json'),
+        child: Lottie.asset('assets/imagens/animation.json'),
       ),
     );
   }
