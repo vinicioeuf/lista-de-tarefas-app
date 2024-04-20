579ffe383175bf8b81c13917f3b8e049d487275b
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-04-20 12:15:08 -0300

    Update add_page.dart

--- lista_de_tarefas/lib/pages/add_page.dart
+++ lista_de_tarefas/lib/pages/add_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AddPage extends StatefulWidget {
-  const AddPage({Key? key}) : super(key: key);
+  const AddPage({super.key});
 
   @override
   State<AddPage> createState() => _AddPageState();
@@ -10,8 +10,14 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   final nomeDaTarefa = TextEditingController();
   final descricaoDaTarefa = TextEditingController();
+  final dataTarefa = TextEditingController();
   final segunda = TextEditingController();
+  final terca = TextEditingController();
+  final quarta = TextEditingController();
+  final quinta = TextEditingController();
+  final sexta = TextEditingController();
   bool showNextFields = false;
+  TimeOfDay _selectedTime = TimeOfDay.now();
 
   @override
   Widget build(BuildContext context) {
@@ -24,151 +30,195 @@ class _AddPageState extends State<AddPage> {
         backgroundColor: Color.fromARGB(255, 25, 25, 25),
       ),
       backgroundColor: Color.fromARGB(255, 25, 25, 25),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Nome do cronograma',
-                filled: true,
-                fillColor: Color.fromARGB(255, 45, 66, 99),
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(22),
-                  borderSide: BorderSide.none,
-                ),
-                suffixIcon: Tooltip(
-                  message:
-                      "Aqui é o nome do cronograma, como ele irá aparecer na página inicial",
-                  child: IconButton(
-                    color: Color.fromARGB(255, 200, 75, 49),
-                    icon: Icon(Icons.help),
-                    onPressed: () {},
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                if (!showNextFields) ...[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Nome do cronograma',
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 45, 66, 99),
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22),
+                        borderSide: BorderSide.none,
+                      ),
+                      suffixIcon: Tooltip(
+                        message:
+                            "Aqui é o nome do cronograma, como ele ira aparecer na página inicial",
+                        child: IconButton(
+                          color: Color.fromARGB(255, 200, 75, 49),
+                          icon: Icon(Icons.help),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                    controller: nomeDaTarefa,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "O nome da tarefa para continuar";
+                      }
+                      return null;
+                    },
                   ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Descrição da tarefa',
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 45, 66, 99),
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22),
+                        borderSide: BorderSide.none,
+                      ),
+                      suffixIcon: Tooltip(
+                        message: "Insira aqui uma breve descrição da tarefa.",
+                        child: IconButton(
+                          color: Color.fromARGB(255, 200, 75, 49),
+                          icon: Icon(Icons.help),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                    controller: descricaoDaTarefa,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Informe a descrição da tarefa para continuar";
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+                SizedBox(
+                  height: 20,
+                ),
+                // Outros campos podem ser adicionados aqui se necessário
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20.0,
+            right: 20.0,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFFC84B31)),
+                minimumSize: MaterialStateProperty.all<Size>(
+                  Size(0.4 * MediaQuery.of(context).size.width, 50),
                 ),
               ),
-              controller: nomeDaTarefa,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "O nome da tarefa para continuar";
-                }
-                return null;
+              child: Text("Próximo", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                setState(() {
+                  showNextFields = !showNextFields;
+                });
               },
             ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Descrição da tarefa',
-                filled: true,
-                fillColor: Color.fromARGB(255, 45, 66, 99),
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(22),
-                  borderSide: BorderSide.none,
-                ),
-                suffixIcon: Tooltip(
-                  message: "Insira aqui uma breve descrição da tarefa.",
-                  child: IconButton(
-                    color: Color.fromARGB(255, 200, 75, 49),
-                    icon: Icon(Icons.help),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-              controller: descricaoDaTarefa,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Informe a descrição da tarefa para continuar";
-                }
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Visibility(
-              visible: showNextFields,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: 50,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Color(0xFF2D4263)),
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Theme(
-                          data: ThemeData(
-                            dialogBackgroundColor: Color(0xFF2D4263),
-                            textTheme: Theme.of(context).textTheme.apply(
-                                  bodyColor: Colors.white,
-                                ),
-                          ),
-                          child: AlertDialog(
-                            title: Text("Segunda Feira"),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Descrição do dia',
-                                    labelStyle: TextStyle(color: Colors.white),
-                                    border: OutlineInputBorder(),
-                                  ),
-                                  controller: segunda,
-                                  // Adicione a lógica de validação aqui, se necessário
-                                ),
-                              ],
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                child: Text(
-                                  "Salvar",
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 200, 75, 49),
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
+          ),
+          Visibility(
+            visible: showNextFields,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.8, // 80% da largura da tela
+                  height: 50, // Altura de 50
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF2D4263)),
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Theme(
+                              data: ThemeData(
+                                // Defina o tema para o AlertDialog
+                                dialogBackgroundColor: Color(
+                                    0xFF2D4263), // Cor de fundo do AlertDialog
+                                textTheme: Theme.of(context).textTheme.apply(
+                                      bodyColor: Colors
+                                          .white, // Cor do texto do AlertDialog
+                                    ),
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  child: Text(
-                    "Segunda Feira",
-                    style: TextStyle(color: Colors.white),
+                              child: AlertDialog(
+  title: Text("Segunda Feira"),
+  content: Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Descrição do dia',
+          labelStyle: TextStyle(color: Colors.white),
+          border: OutlineInputBorder(),
+        ),
+        controller: segunda,
+        // Adicione a lógica de validação aqui, se necessário
+      ),
+      SizedBox(height: 10),
+      Row(
+        children: [
+          Text("Escolha o horário: "),
+          TextButton(
+            onPressed: () async {
+              final TimeOfDay? picked = await showTimePicker(
+                context: context,
+                initialTime: _selectedTime,
+              );
+              if (picked != null && picked != _selectedTime) {
+                setState(() {
+                  _selectedTime = picked;
+                });
+              }
+            },
+            child: Builder(
+              builder: (BuildContext context) {
+                return Text(
+                  _selectedTime.format(context),
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 200, 75, 49),
+                    fontSize: 16.0,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    ]
+  )
+)
+
+                              );
+                        },
+                      );
+                    },
+                    child: Text("Segunda Feira",
+                        style: TextStyle(color: Colors.white)),
                   ),
                 ),
-              ),
+
+                // Adicione outros botões aqui se necessário
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            showNextFields = !showNextFields;
-          });
-        },
-        child: Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
